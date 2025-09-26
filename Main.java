9fce858c782a8298cde1166a473592f861f0b89e
RamithH35 <ramithh18@gmail.com>
2025-09-26 18:15:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        int a,b;
+        for(a=0;a<=5;a++){
+            for(b=0;b<=a;b++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    
+
+    }
+
+    
+}
